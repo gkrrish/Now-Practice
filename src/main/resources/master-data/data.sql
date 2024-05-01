54ca1ca620773b2e6d6aa9c548042c6c2a272d28
Gaganam Krishna <36321398+gkrrish@users.noreply.github.com>
2024-05-01 16:20:25 +0530

    Update data.sql

--- src/main/resources/master-data/data.sql
+++ src/main/resources/master-data/data.sql
@@ -1,28 +1,21 @@
-INSERT INTO MASTER_COUNTRIES (country_id, country_name, telephone_code)VALUES(1, 'United States', '+1');
-INSERT INTO MASTER_COUNTRIES (country_id, country_name, telephone_code)VALUES(2, 'India', '+91');
-
 INSERT INTO MASTER_INDIAN_NEWSPAPER_LANGUAGES (language_id, language_name) VALUES (1, 'English');
 INSERT INTO MASTER_INDIAN_NEWSPAPER_LANGUAGES (language_id, language_name) VALUES (2, 'Hindi');
 INSERT INTO MASTER_INDIAN_NEWSPAPER_LANGUAGES (language_id, language_name) VALUES (3, 'Telugu');
-INSERT INTO MASTER_INDIAN_NEWSPAPER_LANGUAGES (language_id, language_name) VALUES (4, 'Tamil');
+
+INSERT INTO MASTER_COUNTRIES (country_id, country_name, telephone_code)VALUES(1, 'United States', '+1');
+INSERT INTO MASTER_COUNTRIES (country_id, country_name, telephone_code)VALUES(2, 'India', '+91');
 
 INSERT INTO MASTER_STATES (state_id, state_name, country_id)VALUES(1, 'Telangana', 2);
     
+INSERT INTO MASTER_DISTRICTS (district_id, district_name, state_id)VALUES(1, 'Hyderabad', 1);    
+INSERT INTO MASTER_DISTRICTS (district_id, district_name, state_id)VALUES(2, 'Wanaparthy', 1);
 
+INSERT INTO MASTER_MANDALS (mandal_id, mandal_name, district_id)VALUES(1, 'Hyderabad', 1);
+INSERT INTO MASTER_MANDALS (mandal_id, mandal_name, district_id)VALUES(2, 'Ghatkesar', 1);
+INSERT INTO MASTER_MANDALS (mandal_id, mandal_name, district_id)VALUES(3, 'Gopalpet', 2);
+INSERT INTO MASTER_MANDALS (mandal_id, mandal_name, district_id)VALUES(4, 'Wanaparthy', 2);
 
-INSERT INTO MASTER_TELANGANA_DISTRICTS (district_id, district_name, state_id)VALUES(1, 'Hyderabad', 1);    
-INSERT INTO MASTER_TELANGANA_DISTRICTS (district_id, district_name, state_id)VALUES(2, 'Wanaparthy', 1);
-
-
-
-INSERT INTO MASTER_TELANGANA_MANDALS (mandal_id, mandal_name, district_id)VALUES(1, 'Hyderabad', 1);
-INSERT INTO MASTER_TELANGANA_MANDALS (mandal_id, mandal_name, district_id)VALUES(2, 'Ghatkesar', 1);
-INSERT INTO MASTER_TELANGANA_MANDALS (mandal_id, mandal_name, district_id)VALUES(3, 'Gopalpet', 2);
-INSERT INTO MASTER_TELANGANA_MANDALS (mandal_id, mandal_name, district_id)VALUES(4, 'Wanaparthy', 2);
-
-
-
-INSERT INTO MASTER_STATEWISE_TELANGANA_LOCATIONS (
+INSERT INTO MASTER_STATEWISE_LOCATIONS (
     location_id,
     location_name,
     country_id,
@@ -37,6 +30,7 @@ INSERT INTO MASTER_STATEWISE_TELANGANA_LOCATIONS (
     1,  -- District ID for the district
     1   -- Mandal ID for the mandal
 );
+
 
 INSERT INTO USER_DETAILS(UserID, Username, Age, Gender, Location, RegistrationDate, Active,mobilenumber)
 VALUES (1, 'Krishna', 30, 'Male', 'Hyderabad', TIMESTAMP '2024-04-27 10:00:00', 'Y','+919876543210');
@@ -53,24 +47,24 @@ CONNECT BY LEVEL <= 48;
 
 COMMIT;
 ----------------------------------------------
-
-INSERT INTO VENDOR_TELANGANA_EENADU (newspaper_id, location_id, newspaper_name, newspaper_language, subscription_type_id)
-VALUES (1, 1, 'Telangana Eenadu-Hyderabad', 3, 1);
-
-INSERT INTO VENDOR_TELANGANA_VAARTHA (newspaper_id, location_id, newspaper_name, newspaper_language, subscription_type_id)
-VALUES (101, 1, 'Telangana Vaartha', 1, 1);
+INSERT INTO MASTER_Category_Type (category_id, category_name) VALUES (1, 'Local News');
+INSERT INTO MASTER_Category_Type (category_id, category_name) VALUES (2, 'Politics');
 
 
+INSERT INTO SUBSCRIPTION_TYPE (subscriptiontypeid, subscriptiontype, subscriptionduration, subscriptionfee)VALUES (1, 'FREE', '1 month', 0);
+INSERT INTO SUBSCRIPTION_TYPE (subscriptiontypeid, subscriptiontype, subscriptionduration, subscriptionfee)VALUES (2, 'PAID', '3 months', 30.00);
+
+INSERT INTO VENDOR_DETAILS (vendorid, vendorname, newspaper_language, vendorcontactdetails, vendorstatus)VALUES (1, 'EENADU-PUBLICATIONS', 3, 'SOMAJIGUDA-HYDERABAD-TELANGANA-CONTACT EMAIL FOR MORE DETAILS', 'active');
+INSERT INTO VENDOR_DETAILS (vendorid, vendorname, newspaper_language, vendorcontactdetails, vendorstatus)VALUES (2, 'ANDHRAJYOTHI-PUBLICATIONS', 3, 'BANJARAHILS-HYDERABAD-TELANGANA-CONTACT DIRECTLY', 'active');
+INSERT INTO VENDOR_DETAILS (vendorid, vendorname, newspaper_language, vendorcontactdetails, vendorstatus)VALUES (3, 'VAARTHA-PUBLICATIONS', 3, 'LOWER-TANKBUND-HYDERABAD-TELANGANA', 'inactive');
 
 
-
-INSERT INTO VENDOR_DYNAMIC_GENERIC (dynamic_id, telangana_eenadu_id)
-VALUES (1, 1);
-
-
-INSERT INTO UX_USER_SUBSCRIPTION (user_id, newspaper_id, mandal_id, batch_id, user_eligible)
-VALUES (1, 1, 1, 1, 1);
+INSERT INTO VENDORS (newspaper_id, location_id, newspaper_name, newspaper_language, subscription_type_id, publication_type, vendor_id)VALUES (1, 1, 'Eenadu', 3, 1, 'Newspaper', 1);
+INSERT INTO VENDORS (newspaper_id, location_id, newspaper_name, newspaper_language, subscription_type_id, publication_type, vendor_id)VALUES (2, 2, 'Eenadu', 3, 2, 'Newspaper', 1);
+INSERT INTO VENDORS (newspaper_id, location_id, newspaper_name, newspaper_language, subscription_type_id, publication_type, vendor_id)VALUES (3, 2, 'Andhrajyothi', 3, 1, 'Newspaper', 2);
 
 
-
-
+INSERT INTO USER_SUBSCRIPTION (user_id, newspaper_id, batch_id, user_eligible, location_id)VALUES (1, 1, 1, 1, 1);
+INSERT INTO USER_SUBSCRIPTION (user_id, newspaper_id, batch_id, user_eligible, location_id)VALUES (1, 2, 2, 1, 2);
+INSERT INTO USER_SUBSCRIPTION (user_id, newspaper_id, batch_id, user_eligible, location_id)VALUES (1, 2, 8, 1, 1);
+INSERT INTO USER_SUBSCRIPTION (user_id, newspaper_id, batch_id, user_eligible, location_id)VALUES (1, 3, 9, 1, 2);
